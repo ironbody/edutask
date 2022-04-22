2ce6a17676a1de7ce2ad4585dac420a5f66724f8
Simon Saberian <simon.saberian@gmail.com>
2022-04-23 01:01:56 +0200

    updated stuff

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -7,9 +7,8 @@ from src.util.dao import DAO
 def dao():
     dao = DAO("test")
     dao.create({
-        "required_string": "test",
+        "required_string": "test2",
         "required_bool": False,
-        "items": ["3","4"],
     })
 
     yield dao
@@ -18,81 +17,32 @@ def dao():
 
 @pytest.mark.lab1
 @pytest.mark.integration
-def test_allValid(dao):
+def test_1(dao):
     data = {
         "required_string": "test",
         "required_bool": False,
-        "items": ["1","2"],
     }
     result = dao.create(data)
 
-    assert result["required_string"] == data["required_string"] and result["required_bool"] == data["required_bool"] and result["items"] == data["items"]
-
-
-@pytest.mark.lab1
-@pytest.mark.integration
-def test_notUniqueItems(dao):
-    data = {
-        "required_string": "Test1",
-        "required_bool": False,
-        "items": ["3","4"],
-    }
-    
-    with pytest.raises(WriteError):
-        dao.create(data)
+    assert result["required_string"] == data["required_string"] and result["required_bool"] == data["required_bool"]
 
 @pytest.mark.lab1
 @pytest.mark.integration
-def test_invalidBSON(dao):
+def test_2(dao):
     data = {
-        "required_string": True,
-        "required_bool": "test",
-        "items": ["1","2"],
-    }
-
-    with pytest.raises(WriteError):
-        dao.create(data)
-
-@pytest.mark.lab1
-@pytest.mark.integration
-def test_invalidBSON_notUniqueItems(dao):
-    data = {
-        "required_string": "true",
-        "required_bool": "test",
-        "items": ["3","4"],
-    }
-
-    with pytest.raises(WriteError):
-        dao.create(data)
-
-@pytest.mark.lab1
-@pytest.mark.integration
-def test_none_valid(dao):
-    data = {
-        "required_bool": "test",
-        "items": ["3","4"],
-    }
-
-    with pytest.raises(WriteError):
-        dao.create(data)
-
-@pytest.mark.lab1
-@pytest.mark.integration
-def test_invalidRequired(dao):
-    data = {
+        "required_string": "test2",
         "required_bool": True,
-        "items": ["1","2"]
     }
-    
+
     with pytest.raises(WriteError):
         dao.create(data)
 
 @pytest.mark.lab1
 @pytest.mark.integration
-def test_invalidRequired_notUniqueItems(dao):
+def test_3(dao):
     data = {
-        "required_string": "Test1",
-        "items": ["3","4"]
+        "required_string": "test",
+        "required_bool": "test",
     }
 
     with pytest.raises(WriteError):
@@ -100,11 +50,10 @@ def test_invalidRequired_notUniqueItems(dao):
 
 @pytest.mark.lab1
 @pytest.mark.integration
-def test_invalidRequired_invalidBSON(dao):
+def test_4(dao):
     data = {
         "required_bool": "test",
-        "items": ["1","2"],
     }
-    
+
     with pytest.raises(WriteError):
         dao.create(data)
